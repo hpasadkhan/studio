89e2861611a52b226ebed5376643b7d11d170568
asad khan <hpasadkhan@gmail.com>
2025-09-18 17:25:34 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         hostname: 'www.usacoinbook.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
